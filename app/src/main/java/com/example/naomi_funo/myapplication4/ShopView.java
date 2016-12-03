ee1fbe88fd83a1f8efea7834a613d9e71d3d172d
Yuuki Itoyama <yuuki.itoyama@e-seikatsu.co.jp>
2016-12-03 17:26:04 +0900

    ショップ画面のスクロールと画像表示ができた

--- app/src/main/java/com/example/naomi_funo/myapplication4/ShopView.java
+++ app/src/main/java/com/example/naomi_funo/myapplication4/ShopView.java
@@ -3,6 +3,7 @@ package com.example.naomi_funo.myapplication4;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -12,15 +13,12 @@ import android.widget.GridView;
  * Created by y-itoyama on 2016/12/03.
  */
 
-public class ShopView extends Activity {
+public class ShopView extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop_view);
 
-
     }
-
-
 
 }
