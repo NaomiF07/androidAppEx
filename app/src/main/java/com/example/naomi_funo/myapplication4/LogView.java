bed18345c2384a3bc17236bca18ced83167e54da
森田 仁美 <moritahitomi@Morita-no-MacBook-Air-2.local>
2016-12-04 10:04:02 +0900

    目標画面の調整

--- app/src/main/java/com/example/naomi_funo/myapplication4/LogView.java
+++ app/src/main/java/com/example/naomi_funo/myapplication4/LogView.java
@@ -4,7 +4,6 @@ package com.example.naomi_funo.myapplication4;
  * Created by naomi_funo on 2016/11/30.
  */
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,35 +11,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 
 public class LogView extends Fragment {
-    //グローバル変数
-    Globals globals;
 
-    private Button button;
+    TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
-            Bundle saveInstanceState){
+            Bundle saveInstanceState) {
 
         View view = inflater.inflate(R.layout.log_tab, container, false);
-        button =(Button)view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (view == button) {
-                    Intent intent = new Intent(view.getContext(), MokuhyoView.class);
-                    startActivityForResult(intent, 0);
-                }
-            }
-        });
-        globals = (Globals) this.getActivity().getApplication();
-        globals.GlobalsAllInit();
-        int mokuhyotime = globals.mokuhyotime;
-        String set_text = "目標　" + mokuhyotime + "分/日";
-        button.setText(set_text);
+        button = (Button) view.findViewById(R.id.button);
+        textView = (TextView) view.findViewById(R.id.textView);
+        editText = (EditText) view.findViewById(R.id.editText);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // エディットテキストのテキストを取得
+                        String text = editText.getText().toString() + "分 /日";
+                        // 取得したテキストを TextView に張り付ける
+                        textView.setText(text);
+
+                    }
+                });
         return view;
     }
 
--- app/src/main/java/com/example/naomi_funo/myapplication4/MokuhyoView.java
+++ app/src/main/java/com/example/naomi_funo/myapplication4/MokuhyoView.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+//エディットテキスト
+import android.widget.EditText;
+import android.text.SpannableStringBuilder;
+import android.view.View;
 
 /**
  * Created by y-itoyama on 2016/12/03.
@@ -21,21 +25,52 @@ public class MokuhyoView extends Activity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mokuhyo_view);
+    }
 
-//        TextView text = (TextView) findViewById(R.id.textView10);
-//        String set_text = "20分 /日";
-//        text.setText(set_text);
+    public void onClick_Button1(View view){
 
         //グローバル変数を取得
         globals = (Globals) this.getApplication();
         //初期化
         globals.GlobalsAllInit();
 
-        //取得した文章をレイアウトに配置
-        TextView text = (TextView) findViewById(R.id.textView10);
-        int mokuhyotime = globals.mokuhyotime;
-        String set_text = mokuhyotime + "分 /日";
-        text.setText(set_text);
+        //ボタンをクリックするとeditTextの数値を取得する
+        EditText editText3 = (EditText)findViewById(R.id.editText3);
+        EditText editText2 = (EditText)findViewById(R.id.editText2);
+
+        String text = editText3.getText().toString();
+        editText2.setText(text);
+
+        globals.mokuhyotime = Integer.parseInt(editText3.getText().toString());
+
 
     }
+
+
+
+
+
+
+//        TextView text = (TextView) findViewById(R.id.textView10);
+//        String set_text = "20分 /日";
+//        text.setText(set_text);
+
+
+
+
+
+//        //取得した文章をレイアウトに配置
+//        TextView text = (TextView) findViewById(R.id.textView10);
+//        int mokuhyotime = globals.mokuhyotime;
+//        String set_text = mokuhyotime + "分 /日";
+//        text.setText(set_text);
+
+
+
+//        int mokuhyotime = globals.mokuhyotime;
+//        editText.setText(mokuhyotime);
+//        editText.selectAll();
+//        globals.mokuhyotime = Integer.parseInt(editText.getText().toString());
+
+
 }
