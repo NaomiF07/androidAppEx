b88434e2fe114fe7ab1b6b421ea1efc23be60dcd
mayu.kitamura <mayu.kitamura@e-seikatsu.co.jp>
2017-10-06 18:40:59 +0900

    refs push 動くよ！

--- app/src/main/java/com/example/a100527/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a100527/myapplication/MainActivity.java
@@ -2,46 +2,44 @@ package com.example.a100527.myapplication;
 
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.view.Menu;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initTabs();
+try {
+    TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+    tabHost.setup();
+    TabHost.TabSpec spec;
+
+    // tab1
+
+    spec = tabHost.newTabSpec("main")
+            .setIndicator("main")
+            .setContent(R.layout.activity_main);
+    tabHost.addTab(spec);
+
+    spec = tabHost.newTabSpec("left")
+            .setIndicator("left")
+            .setContent(R.layout.right_page);
+    tabHost.addTab(spec);
+
+    spec = tabHost.newTabSpec("right")
+            .setIndicator("right")
+            .setContent(R.layout.left_page);
+    tabHost.addTab(spec);
+    tabHost.setCurrentTab(0);
+} catch (RuntimeException e) {
+    e.printStackTrace();
+}
     }
 
-    private void  initTabs() {
-        try {
-            TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
-            tabHost.setup();
-            TabHost.TabSpec spec;
-
-            // tab1
-
-            spec = tabHost.newTabSpec("main")
-                    .setIndicator("home")
-                    .setContent(R.id.main);
-            tabHost.addTab(spec);
-
-            spec = tabHost.newTabSpec("left")
-                    .setIndicator("left")
-                    .setContent(R.id.left);
-            tabHost.addTab(spec);
-
-            spec = tabHost.newTabSpec("right")
-                    .setIndicator("right")
-                    .setContent(R.id.right);
-            tabHost.addTab(spec);
-            tabHost.setCurrentTab(0);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
